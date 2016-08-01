047a963e943dbdcbb1d45cfedd7d516f85cc30f9
Anastasiia <___@gmail.com>
2016-08-01 10:30:16 +0200

    add files

---
+++ third-app/src/main/java/Third/app/App.java
@@ -0,0 +1,13 @@
+package Third.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi! I am third application" );
+    }
+}
